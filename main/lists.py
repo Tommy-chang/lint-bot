da2165a5ec1aa5b42718a0f362aa14c628ab5ce4
Tommy-chang <85210510+Tommy-chang@users.noreply.github.com>
2023-06-25 11:05:08 +0800

    user_id_list_updated

--- main/lists.py
+++ main/lists.py
@@ -5,9 +5,6 @@ options = [
     "不要罵髒話",
     "DO NOT SAY BAD WORDS!!!"
 ]
-try:
-    user_list = db.session.query(user_info.user_id).all()
-except:
-    pass
+user_list = db.session.query(user_info.user_id).all()
 n_send_options = ["Hi there, it is 9:30, time to sleep! zzz", "The robot is too quiet? Never say that again.", "Too boring? Then chat with me!"]
 m_send_options = ["Good morning!", "It is beginning of a wonderful day!"]
